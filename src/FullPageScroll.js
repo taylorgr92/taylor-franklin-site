acf716e0aa62db5053e5b3c72349e5199120b120
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2022-06-23 20:33:39 -0500

    Home section card

--- src/FullPageScroll.js
+++ src/FullPageScroll.js
@@ -1,41 +1,47 @@
 import React from "react";
-import ReactFullpage ,{pluginWrapper} from "@fullpage/react-fullpage";
-
+import ReactFullpage, { pluginWrapper } from "@fullpage/react-fullpage";
+import HomeCard from "./homeSect.js"
 
 const FullPageScroll = () => (
-    <ReactFullpage
-      pluginWrapper = {pluginWrapper}
-  
-    
-      scrollingSpeed = {1000} /* Options here */
-      scrollHorizontally = {true}  /* Because we are using the extension */
-    
-  
-      render={({ state, fullpageApi }) => {
-        return (
-          <ReactFullpage.Wrapper>
-            <div className="section">
-              <p>Section 1 </p>
-              <button onClick={() => fullpageApi.moveSectionDown()}>
-                Click me to move down
-              </button>
-            </div>
-            <div className="section">
-              <p>Section 2</p>
-              <button onClick={() => fullpageApi.moveSectionDown()}>
-                Click me to move down
-              </button>
-            </div>
-            <div className="section">
-              <p>Section 3</p>
-              <button onClick={() => fullpageApi.moveSectionUp()}>
-                Click me to move up
-              </button>
-            </div>
-          </ReactFullpage.Wrapper>
-        );
-      }}
-    />
-  );
+  <ReactFullpage
+    pluginWrapper={pluginWrapper}
+    scrollingSpeed={1000} /* Options here */
+    scrollHorizontally={true} /* Because we are using the extension */
+    render={({ state, fullpageApi }) => {
+      return (
+        <>
+        <ReactFullpage.Wrapper>
+
+          <div className="section home">
+          
+          <HomeCard/>
+          </div>
+
+          <div className="section skills">
+            <p>Section 2</p>
+            <button onClick={() => fullpageApi.moveSectionDown()}>
+              Click me to move down
+            </button>
+          </div>
+
+          <div className="section work">
+            <p>Section 3</p>
+            <button onClick={() => fullpageApi.moveSectionDown()}>
+              Click me to move down
+            </button>
+          </div>
+
+          <div className="section contact">
+            <p>Section 4</p>
+            <button onClick={() => fullpageApi.moveSectionUp()}>
+              Click me to move down
+            </button>
+          </div>
+        </ReactFullpage.Wrapper>
+        </>
+      );
+    }}
+  />
+);
 
 export default FullPageScroll;
